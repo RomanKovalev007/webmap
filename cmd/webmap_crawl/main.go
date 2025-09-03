faef0f0467650f595839e13db896f35506dc6bea
RomanKovalev007 <romakov640@gmail.com>
2025-09-03 23:53:00 +0300

    рабочий, но неправильный выход

--- cmd/webmap_crawl/main.go
+++ cmd/webmap_crawl/main.go
@@ -5,7 +5,6 @@ import (
 	"fmt"
 	"net/url"
 	"sync"
-	"sync/atomic"
 	"time"
 
 	"github.com/RomanKovalev007/webmap/internal/worker"
@@ -18,7 +17,14 @@ func main() {
 	maxWorkers := 5
 	maxDepth := 2
 	maxPages := 500
-	timeout := 1 * time.Second
+	timeout := 5 * time.Second
+
+	var mutex sync.Mutex
+
+	visited := make(map[string]bool)
+	mutex.Lock()
+	visited[startURL] = true
+	mutex.Unlock()
 
 	ctx, cancel := context.WithTimeout(context.Background(), timeout)
 	defer cancel()
@@ -28,82 +34,49 @@ func main() {
 		fmt.Printf("Invalid start URL: %v\n", err)
 		return
 	}
-
 	baseDomain := parsedURL.Hostname()
 
-	tasks := make(chan worker.CrawlTask, 100)
-	results := make(chan worker.CrawlTask, 100)
+	tasks := make(chan worker.CrawlTask, 1000)
+	results := worker.WorkerPool(ctx, tasks, maxDepth, baseDomain, maxWorkers)
 
-	visited := make(map[string]bool)
 
-	var mutex sync.Mutex
-	var wg sync.WaitGroup
-
-	var activeTasks int32 = 1
-
-	// Отправка начальной задачи
 	tasks <- worker.CrawlTask{
 		URL: startURL,
 		Depth: 0,
 	}
 	fmt.Println("первая задача записана в таски")
 
-	// Запуск воркеров
+	for task := range results{
+		//fmt.Println("MAIN пришла из результатов ", task)			
+		mutex.Lock()
+		if len(visited) > maxPages{
+			mutex.Unlock()
+			cancel()
+			close(tasks)
+			fmt.Println("канал задач закрылся")
+			break 
+		}
+		
+		if task.Depth > maxDepth {
+			mutex.Unlock()
+			continue
+		}
 
-	for i := 1; i <= maxWorkers; i++ {
-		wg.Add(1)
-		fmt.Println("воркер запущен ", i)
-			time.Sleep(100 * time.Microsecond)
-		go worker.Worker(ctx, tasks, results, &wg, maxDepth, baseDomain)
+		if visited[task.URL]{
+			mutex.Unlock()
+			continue
+		}
+		visited[task.URL] = true
+		mutex.Unlock()
+
+
+		tasks <- task
+		//fmt.Println("MAIN отправилась в таски ", task)
 	}
 
-	// Обработка результатов
-    go func() {
-        for {
-			select {
-			case <-ctx.Done():
-				return
-			case task, ok := <-results:
-				if !ok {
-					return
-				}
-
-            	mutex.Lock()
-				shouldSend := !visited[task.URL] && len(visited) <= maxPages
-				mutex.Unlock()
-
-				if shouldSend {
-					mutex.Lock()
-					visited[task.URL] = true
-					mutex.Unlock()
-					
-					fmt.Printf("Found %s (Depth %d)\n", task.URL, task.Depth)
-					atomic.AddInt32(&activeTasks, 1)
-
-					select {
-					case <-ctx.Done():
-					return
-					case tasks <- task:
+	fmt.Println("Найдено страниц: ", len(visited))
 }
-            }
-            
-        	}
-		}
-    }()
 
-	go func() {
-		for range tasks {
-			if atomic.AddInt32(&activeTasks, -1) == 0 {
-				close(tasks)
-			}
-		}
-	}()
-	
-	
-	wg.Wait()
-	close(results)
-	
-	fmt.Println("Crawling finished!")
-	fmt.Println("Visited pages:", len(visited))
 
-}
+
+	
